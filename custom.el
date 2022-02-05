4851234ca51490ff107cef9202c2272ca4b5d3c6
Jared-Mullin <Jaredmullin@protonmail.com>
2022-02-05 16:02:36 +0000

    Doom Emacs Configuration

---
+++ custom.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "353ffc8e6b53a91ac87b7e86bebc6796877a0b76ddfc15793e4d7880976132ae" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
+ '(exwm-floating-border-color "#252630")
+ '(fci-rule-color "#9099c0")
+ '(highlight-tail-colors ((("#222e36") . 0) (("#29313b") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#414868" "#b4f9f8"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#414868" "#73daca"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#414868" "#8189af"))
+ '(objed-cursor-color "#f7768e")
+ '(pdf-view-midnight-colors (cons "#a9b1d6" "#1a1b26"))
+ '(rustic-ansi-faces
+   ["#1a1b26" "#f7768e" "#73daca" "#e0af68" "#7aa2f7" "#bb9af7" "#b4f9f8" "#a9b1d6"])
+ '(vc-annotate-background "#1a1b26")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#73daca")
+    (cons 40 "#97cba9")
+    (cons 60 "#bbbd88")
+    (cons 80 "#e0af68")
+    (cons 100 "#eaa966")
+    (cons 120 "#f4a365")
+    (cons 140 "#ff9e64")
+    (cons 160 "#e89c94")
+    (cons 180 "#d19bc5")
+    (cons 200 "#bb9af7")
+    (cons 220 "#cf8ed4")
+    (cons 240 "#e382b1")
+    (cons 260 "#f7768e")
+    (cons 280 "#d97a96")
+    (cons 300 "#bc7f9e")
+    (cons 320 "#9e84a6")
+    (cons 340 "#9099c0")
+    (cons 360 "#9099c0")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
